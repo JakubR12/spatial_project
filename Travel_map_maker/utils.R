228b428c6fc28694af9f4a0057e368c12779089d
au604148 <au604148@uni.au.dk>
2021-06-04 13:06:30 +0200

    error messages

--- Travel_map_maker/utils.R
+++ Travel_map_maker/utils.R
@@ -34,35 +34,36 @@ error_check <- function(df, label_col) {
   len = length(df[, 1])
   
   if (!sum(c("lat", "long") %in% colnames(df)) == 2) {
-    alert(title = "Error !!",
-          text = "it seems that you have uchosen the wrong seperator. Try and check the box with the right seperator for your data",
+    alert(title = "Separator Error !!",
+          text = "It seems that you have chosen a wrong seperator. You see the dataset below the map and choose a corresponding separator.",
           type = "error")
     
   }
   else if (!sum(check.numeric(df$long) + check.numeric(df$lat)) == len * 2) {
-    alert(title = "Error !!",
-          text = "not numeric",
+    alert(title = "Longitude or Latitude Class Error !!",
+          text = "It seems that some of your values in long or lat columns are not numeric. Double check their formats.",
           type = "warning")
   }
   else if (!sum(df$long <= 180 & df$long >= -180) == len) {
-    alert(title = "Error !!",
-          text = "longitude error",
+    alert(title = "Longitude Error !!",
+          text = "It seems that your long column contains a value outside of the allowed range for longitude (-180 till 180). Ensure that all values are within the range.",
           type = "error")
   }
   
   else if (!sum(df$lat <= 90 & df$lat >= -90) == len) {
-    alert(title = "Error !!",
-          text = "latitude error",
+    alert(title = "Latitude Error !!",
+          text = "It seems that your lat column contains a value outside of the allowed range for latitude (-90 till 90). More often that not, this is caused by flipping long and lat columns. Ensure that all values are within the range.",
           type = "error")
   }
   else if (ncol(select_if(df, names(df) %in% label_col)) != 1) {
-    alert(title = "Error !!",
-          text = "Label Name Error",
+    alert(title = "Label Error !!",
+          text = "It seems that your column name for labellin/describing locations does not correspond to the default: Description. You can change it in the Upload options in the Label name box.",
           type = "error")
   }
   else {
     alert(title = "Success !!",
-          text = "All in order",
+          text = "Your data is loaded in the right format, now you can customize the asthetics!",
           type = "success")
   }
 }
+
