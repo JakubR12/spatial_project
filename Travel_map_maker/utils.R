4ca6e366dbcc3350aba683f342c8a8069ec9138e
Peter Thramkrongart <Peter.Thramkrongart@gmail.com>
2021-06-04 13:11:41 +0200

    fixed some spelling errors

--- Travel_map_maker/utils.R
+++ Travel_map_maker/utils.R
@@ -35,24 +35,24 @@ error_check <- function(df, label_col) {
   
   if (!sum(c("lat", "long") %in% colnames(df)) == 2) {
     alert(title = "Separator Error !!",
-          text = "It seems that you have chosen a wrong seperator. You see the dataset below the map and choose a corresponding separator.",
+          text = "It seems that you have chosen a wrong seperator. You see can the dataset below the map and choose a corresponding separator.",
           type = "error")
     
   }
   else if (!sum(check.numeric(df$long) + check.numeric(df$lat)) == len * 2) {
     alert(title = "Longitude or Latitude Class Error !!",
-          text = "It seems that some of your values in long or lat columns are not numeric. Double check their formats.",
+          text = "It seems that some of your values in the long or lat columns are not numeric. Double check their formats.",
           type = "warning")
   }
   else if (!sum(df$long <= 180 & df$long >= -180) == len) {
     alert(title = "Longitude Error !!",
-          text = "It seems that your long column contains a value outside of the allowed range for longitude (-180 till 180). Ensure that all values are within the range.",
+          text = "It seems that your long column contains a value outside of the allowed range for longitude (-180 - 180). Ensure that all values are within the range.",
           type = "error")
   }
   
   else if (!sum(df$lat <= 90 & df$lat >= -90) == len) {
     alert(title = "Latitude Error !!",
-          text = "It seems that your lat column contains a value outside of the allowed range for latitude (-90 till 90). More often that not, this is caused by flipping long and lat columns. Ensure that all values are within the range.",
+          text = "It seems that your lat column contains a value outside of the allowed range for latitude (-90 - 90). More often that not, this is caused by flipping long and lat columns. Ensure that all values are within the range.",
           type = "error")
   }
   else if (ncol(select_if(df, names(df) %in% label_col)) != 1) {
@@ -62,7 +62,7 @@ error_check <- function(df, label_col) {
   }
   else {
     alert(title = "Success !!",
-          text = "Your data is loaded in the right format, now you can customize the asthetics!",
+          text = "Your data is loaded in the right format, now you can customize the aesthetics!",
           type = "success")
   }
 }
